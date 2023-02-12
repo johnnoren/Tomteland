ea7d016535e2caf417eedc9fdfc93c7ab5428020
John Norén <super.happy.email.fun@gmail.com>
2023-02-12 19:58:18 +0100

    Add nodes data

--- app/src/main/java/com/example/Tomteland.kt
+++ app/src/main/java/com/example/Tomteland.kt
@@ -7,15 +7,18 @@ class Tomteland {
         val nodes = Node<String>("Tomten")
         nodes.add(Node("Glader"),"Tomten")
         nodes.add(Node("Butter"),"Tomten")
+        nodes.add(Node("Tröger"),"Glader")
+        nodes.add(Node("Trötter"),"Glader")
+        nodes.add(Node("Blyger"),"Glader")
+        nodes.add(Node("Rådjuret"),"Butter")
+        nodes.add(Node("Nyckelpigan"),"Butter")
+        nodes.add(Node("Haren"),"Butter")
+        nodes.add(Node("Räven"),"Butter")
+        nodes.add(Node("Skumtomten"),"Trötter")
+        nodes.add(Node("Dammråttan"),"Skumtomten")
+        nodes.add(Node("Gråsuggan"),"Räven")
+        nodes.add(Node("Myran"),"Räven")
+        nodes.add(Node("Bladlusen"),"Myran")
+        this.nodes = nodes
     }
 }
-
-
-/*Högsta chefen för allt är "Tomten"
-Under "Tomten" jobbar "Glader" och "Butter"
-Under "Glader" jobbar "Tröger", "Trötter" och "Blyger"
-Under "Butter" jobbar "Rådjuret", "Nyckelpigan", "Haren" och "Räven"
-Under "Trötter" jobbar "Skumtomten"
-Under "Skumtomten" jobbar "Dammråttan"
-Under "Räven" jobbar "Gråsuggan" och "Myran"
-Under "Myran" jobbar "Bladlusen"*/
